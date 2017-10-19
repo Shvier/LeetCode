932eb996ae2b29cbbe79ed7a79696231ae1c749f
Shvier <daulvi@hotmail.com>
2017-10-19 23:09:46 +0800

    Add Problem 7 Solution

--- 7-ReverseInteger/main.swift
+++ 7-ReverseInteger/main.swift
@@ -10,6 +10,40 @@ import Foundation
 
 class Solution {
     static func reverse(_ x: Int) -> Int {
-        return 0
+        var result: Int = 0
+        var numbers = Array<Int>()
+        var step: Int = 10
+        var temp = Int(fabs(Double(x)))
+        while true {
+            if temp >= step {
+                numbers.append(temp%step)
+            } else {
+                numbers.append(temp)
+                break
+            }
+            temp /= step
+        }
+        step = 1
+        for _ in 0..<numbers.count - 1 {
+            step *= 10
+        }
+        for number in numbers {
+            result += number*step
+            step /= 10
+        }
+        let max = Int(INT32_MAX)
+        if x < 0 {
+            if Int(fabs(Double(result))) > max {
+                return 0
+            }
+            return 0-result
+        } else {
+            if result > max {
+                return 0
+            }
+            return result
+        }
     }
 }
+
+print(Solution.reverse(1534236469))
