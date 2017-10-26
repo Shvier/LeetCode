4780d5383c9c21c54d925f2766fd45c789bd5ef6
Shvier <daulvi@hotmail.com>
2017-10-26 17:33:31 +0800

    Init Problem 11

---
+++ 11-ContainerWithMostWater/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  11-ContainerWithMostWater
+//
+//  Created by Shvier on 26/10/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    static func maxArea(_ height: [Int]) -> Int {
+        
+        return 0
+    }
+    
+}
