4fdaadaaf2913838cf41ecfe2576e53f3c63563a
Shvier <daulvi@hotmail.com>
2017-10-27 17:02:15 +0800

    Add Problem 11 Solution

--- 11-ContainerWithMostWater/main.swift
+++ 11-ContainerWithMostWater/main.swift
@@ -11,8 +11,18 @@ import Foundation
 class Solution {
     
     static func maxArea(_ height: [Int]) -> Int {
-        
-        return 0
+        var left = 0
+        var right = height.count - 1
+        var maxArea: Int = 0
+        while left < right {
+            maxArea = max(maxArea, min(height[left], height[right])*(right - left))
+            if height[left] < height[right] {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+        return maxArea
     }
     
 }
