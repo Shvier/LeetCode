08699ad0301919969062565c2f3833dfbee1eb70
Shvier <daulvi@hotmail.com>
2017-11-04 17:19:25 +0800

    Add Problem 16

---
+++ 16-3SumClosest/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  16-3SumClosest
+//
+//  Created by Shvier on 04/11/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    static func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
+        return 0
+    }
+    
+}
