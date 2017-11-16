db99b975d9746c0b2433e3fee102e33d41d31afb
Shvier <daulvi@hotmail.com>
2017-11-16 22:32:56 +0800

    Add Problem 20

---
+++ 20-ValidParentheses/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  20-ValidParentheses
+//
+//  Created by Shvier on 16/11/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    static func isValid(_ s: String) -> Bool {
+        
+        return true
+    }
+    
+}
