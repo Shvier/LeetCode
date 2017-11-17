ca40a76c969170a55aa95032d8e8c7bc8acfe7f6
Shvier <daulvi@hotmail.com>
2017-11-17 17:34:14 +0800

    Add Problem 20 Solution Need bug fix

--- 20-ValidParentheses/main.swift
+++ 20-ValidParentheses/main.swift
@@ -11,8 +11,27 @@ import Foundation
 class Solution {
     
     static func isValid(_ s: String) -> Bool {
-        
-        return true
+        var parentthesesCount: Int = 0
+        var bracketCount: Int = 0
+        var braceCount: Int = 0
+        let characters = [Character](s.characters)
+        for character in characters {
+            let string = String(character)
+            if string == "(" {
+                parentthesesCount += 1
+            } else if string == ")" {
+                parentthesesCount -= 1
+            } else if string == "[" {
+                bracketCount += 1
+            } else if string == "]" {
+                bracketCount -= 1
+            } else if string == "{" {
+                braceCount += 1
+            } else if string == "}" {
+                braceCount -= 1
+            }
+        }
+        return (parentthesesCount == 0) && (bracketCount == 0) && (braceCount == 0)
     }
     
 }
