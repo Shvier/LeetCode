aafe39c4e266a327134e4d3ace5c1586312e4209
Shvier <daulvi@hotmail.com>
2017-12-02 18:38:33 +0800

    Add Problem 32

---
+++ 31-NextPermutation/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  31-NextPermutation
+//
+//  Created by Shvier on 02/12/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func nextPermutation(_ nums: inout [Int]) {
+        if nums.count < 2 {
+            return
+        }
+        
+    }
+    
+    func checkSmallerNumber(_ nums: inout [Int], _ left: Int, _ right: Int) {
+        
+    }
+    
+}
