22552ff03fb07e78b533bd9c799bb291f188d14a
Shvier <daulvi@hotmail.com>
2017-12-12 22:30:59 +0800

    Add Problem 37

---
+++ 37-SudokuSolver/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  37-SudokuSolver
+//
+//  Created by Shvier on 11/12/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func solveSudoku(_ board: inout [[Character]]) {
+        
+    }
+    
+}
