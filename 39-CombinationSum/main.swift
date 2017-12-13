49209357f7c53f83406c033fe14a4520a120118e
Shvier <daulvi@hotmail.com>
2017-12-13 22:01:32 +0800

    Add Problem 39 Solution

---
+++ 39-CombinationSum/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  39-CombinationSum
+//
+//  Created by Shvier on 13/12/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var sortedCandidates = candidates.sorted()
+        var result = Array<Array<Int>>()
+        let current = Array<Int>()
+        backtrack(&result, &sortedCandidates, current: current, lastUse: 0, restTarget: target)
+        return result
+    }
+    
+    func backtrack(_ result: inout [[Int]], _ candidates: inout [Int], current: [Int], lastUse: Int, restTarget: Int) {
+        if restTarget == 0 {
+            result.append(current)
+        }
+        var newCurrent = current
+        for i in lastUse..<candidates.count {
+            if candidates[i] > restTarget {
+                break
+            }
+            newCurrent.append(candidates[i])
+            backtrack(&result, &candidates, current: newCurrent, lastUse: i, restTarget: restTarget-candidates[i])
+            newCurrent.removeLast()
+        }
+    }
+    
+}
