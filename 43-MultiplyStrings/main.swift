90099ca9abf45393fd2cf95833ae7e49fa645b91
Shvier <daulvi@hotmail.com>
2017-12-22 23:39:31 +0800

    Update Problem 43 Solution

--- 43-MultiplyStrings/main.swift
+++ 43-MultiplyStrings/main.swift
@@ -11,38 +11,30 @@ import Foundation
 class Solution {
     
     func multiply(_ num1: String, _ num2: String) -> String {
-        if num1.lengthOfBytes(using: .utf8) < 1 {
-            return num2
-        }
-        if num2.lengthOfBytes(using: .utf8) < 1 {
-            return num1
-        }
-        var characters1 = [Character](num1.characters)
-        var characters2 = [Character](num2.characters)
-        if characters1.count > characters2.count {
-            let temp = characters1
-            characters1 = characters2
-            characters2 = temp
+        let num1Length = num1.lengthOfBytes(using: .utf8)
+        let num2Length = num2.lengthOfBytes(using: .utf8)
+        let characters1 = [Character](num1.characters)
+        let characters2 = [Character](num2.characters)
+        var pos = Array<Int>(repeatElement(0, count: num1Length+num2Length))
+        for i in (0..<num1Length).reversed() {
+            for j in (0..<num2Length).reversed() {
+                let number1 = Int.init(String(characters1[i]))!
+                let number2 = Int.init(String(characters2[j]))!
+                let mul = number1*number2
+                let p1 = i + j
+                let p2 = i + j + 1
+                let sum = mul + pos[p2]
+                pos[p1] += sum / 10
+                pos[p2] = sum % 10
+            }
         }
         var result = ""
-        var carry = 0
-        for index in (0..<characters1.count).reversed() {
-            let number1 = Int.init(String(characters1[index]))!
-            let number2 = Int.init(String(characters2[index]))!
-            var temp = number1*number2
-            temp += carry
-            if temp >= 10 {
-                carry = temp/10
-                temp = temp%10
-            } else {
-                carry = 0
+        for number in pos {
+            if !(result.lengthOfBytes(using: .utf8) == 0 && number == 0) {
+                result = "\(result)\(number)"
             }
-            result = "\(temp)\(result)"
         }
-        if carry > 0 {
-            result = "\(carry)\(result)"
-        }
-        return result
+        return result.lengthOfBytes(using: .utf8) == 0 ? "0" : result
     }
     
 }
