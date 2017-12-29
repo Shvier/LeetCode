c3f9b70f27df752f02bd8a13a5d95290409cf66a
Shvier <daulvi@hotmail.com>
2017-12-29 17:24:03 +0800

    Add Problem 45

---
+++ 45-JumpGameII/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  45-JumpGameII
+//
+//  Created by Shvier on 29/12/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func jump(_ nums: [Int]) -> Int {
+        
+    }
+    
+}
