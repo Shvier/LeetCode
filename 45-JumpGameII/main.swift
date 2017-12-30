bc5ff9a60ca34defcb1b2fcb7dc6e1f91da0f9ac
Shvier <daulvi@hotmail.com>
2017-12-30 18:37:42 +0800

    Update Proboem 45 Solution

--- 45-JumpGameII/main.swift
+++ 45-JumpGameII/main.swift
@@ -11,7 +11,29 @@ import Foundation
 class Solution {
     
     func jump(_ nums: [Int]) -> Int {
-        
+        let count = nums.count
+        if count < 2 {
+            return 0
+        }
+        var currentMax = 0
+        var nextMax = 0
+        var index = 0
+        var level = 0
+        while currentMax >= index {
+            level += 1
+            for i in index...currentMax {
+                nextMax = max(nextMax, nums[i]+i)
+                if nextMax >= count-1 {
+                    return level
+                }
+                index += 1
+            }
+            if currentMax == nextMax {
+                return 0
+            }
+            currentMax = nextMax
+        }
+        return 0
     }
     
 }
