35965183fbcafc60848ef954ba2dfa0d9fbc680f
Shvier <daulvi@hotmail.com>
2018-01-05 19:18:43 +0800

    Save

--- 47-PermutationsII/main.swift
+++ 47-PermutationsII/main.swift
@@ -12,31 +12,29 @@ class Solution {
     
     func permuteUnique(_ nums: [Int]) -> [[Int]] {
         var result = Array<Array<Int>>()
-        var tempResult = Array<String>()
         var temp = Array<Int>()
-        var tempString = ""
         var seen = Array(repeating: false, count: nums.count)
-        backtrack(&result, nums, &seen, &temp, &tempResult, &tempString)
+        backtrack(&result, nums.sorted(), &seen, &temp)
         return result
     }
     
-    func backtrack(_ result: inout [[Int]], _ nums: [Int], _ seen: inout [Bool], _ temp: inout [Int], _ tempResult: inout [String], _ tempString: inout String) {
+    func backtrack(_ result: inout [[Int]], _ nums: [Int], _ seen: inout [Bool], _ temp: inout [Int]) {
         let count = nums.count
         if temp.count == count {
-            if !tempResult.contains(tempString) {
-                result.append(temp)
-                tempResult.append(tempString)
-            }
+            result.append(temp)
         }
         for index in 0..<count {
+            if index > 0 {
+                if nums[index] == nums[index-1] && temp.count == 0 {
+                    continue
+                }
+            }
             if !seen[index] {
                 seen[index] = true
                 temp.append(nums[index])
-                tempString = "\(tempString)\(nums[index])"
-                backtrack(&result, nums, &seen, &temp, &tempResult, &tempString)
+                backtrack(&result, nums, &seen, &temp)
                 seen[index] = false
                 temp.removeLast()
-                tempString = tempString.substring(to: tempString.index(before: tempString.endIndex))
             }
         }
     }
