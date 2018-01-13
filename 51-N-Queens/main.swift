0371f3cde30b0404a0f50d668a385fc4ac97caa3
Shvier <daulvi@hotmail.com>
2018-01-13 21:16:33 +0800

    Update Problem 51 Solution

--- 51-N-Queens/main.swift
+++ 51-N-Queens/main.swift
@@ -13,32 +13,58 @@ class Solution {
     func solveNQueens(_ n: Int) -> [[String]] {
         var ans = Array<Array<String>>()
         var matrix = Array(repeating: Array(repeatElement(".", count: n)), count: n)
-        var validPosition = [(x: Int, y: Int)]()
-        placeQueens(&ans, &matrix, &validPosition)
+        placeQueens(&ans, &matrix, 0)
         return ans
     }
     
-    func placeQueens(_ ans: inout [[String]], _ matrix: inout [[String]], _ validPosition: inout [(x: Int, y: Int)]) {
-        for x in 0..<matrix.count {
-            for y in 0..<matrix.count {
-                if !checkPositionValid(validPosition, (x, y)) || matrix[x][y] == "Q" {
-                    continue
-                }
-                validPosition.append((x, y))
-                matrix[x][y] = "Q"
-                placeQueens(&ans, &matrix, &validPosition)
-                validPosition.removeLast()
+    func placeQueens(_ ans: inout [[String]], _ matrix: inout [[String]], _ row: Int) {
+        let count = matrix.count
+        if row == count {
+            var currentQueens = [String]()
+            for strings in matrix {
+                let string = strings.joined(separator: "")
+                currentQueens.append(string)
+            }
+            ans.append(currentQueens)
+            return
+        }
+        for col in 0..<count {
+            if checkPositionValid(matrix, row, col, count) {
+                matrix[row][col] = "Q"
+                placeQueens(&ans, &matrix, row + 1)
+                matrix[row][col] = "."
             }
         }
     }
     
-    func checkPositionValid(_ position: [(x: Int, y: Int)], _ ord: (x: Int, y: Int)) -> Bool {
-        for pos in position {
-            if pos.x == ord.x && pos.y == ord.y {
+    func checkPositionValid(_ queens: [[String]], _ row: Int, _ col: Int, _ max: Int) -> Bool {
+        for index in 0..<row {
+            if queens[index][col] == "Q" {
                 return false
             }
+        }
+        var i = row - 1
+        var j = col - 1
+        while i >= 0 && j >= 0 {
+            if queens[i][j] == "Q" {
+                return false
+            }
+            i -= 1
+            j -= 1
+        }
+        i = row - 1
+        j = col + 1
+        while i >= 0 && j < max {
+            if queens[i][j] == "Q" {
+                return false
+            }
+            i -= 1
+            j += 1
         }
         return true
     }
     
 }
+
+let solution = Solution()
+print(solution.solveNQueens(8))
