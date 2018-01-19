6112b61e9fda0cab515cbbe32ca40b6403c58050
Shvier <daulvi@hotmail.com>
2018-01-19 18:46:00 +0800

    Add Problem 55

---
+++ 55-JumpGame/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  55-JumpGame
+//
+//  Created by Shvier on 19/01/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func canJump(_ nums: [Int]) -> Bool {
+        
+    }
+    
+}
