92222497b1988750d90b42e0e26ea84330f43372
Shvier <daulvi@hotmail.com>
2018-01-30 23:58:03 +0800

    Update Problem 67 Solution

--- 67-AddBinary/main.swift
+++ 67-AddBinary/main.swift
@@ -13,11 +13,52 @@ class Solution {
     func addBinary(_ a: String, _ b: String) -> String {
         let charactersA = [Character](a.characters)
         let charactersB = [Character](b.characters)
-        let lengthA = charactersA.count
-        let lengthB = charactersB.count
+        var indexA = charactersA.count - 1
+        var indexB = charactersB.count - 1
+        var carried = false
         var ans = ""
-        
+        while indexA >= 0 || indexB >= 0 {
+            var charA = ""
+            var charB = ""
+            if indexA >= 0 {
+                charA = String(charactersA[indexA])
+            }
+            if indexB >= 0 {
+                charB = String(charactersB[indexB])
+            }
+            if carried {
+                if charA == "1" && charB == "1" {
+                    ans = "1\(ans)"
+                    carried = true
+                } else if (charA == "1" && charB != "1") || (charA != "1" && charB == "1") {
+                    ans = "0\(ans)"
+                    carried = true
+                } else {
+                    ans = "1\(ans)"
+                    carried = false
+                }
+            } else {
+                if charA == "1" && charB == "1" {
+                    ans = "0\(ans)"
+                    carried = true
+                } else if (charA == "1" && charB != "1") || (charA != "1" && charB == "1") {
+                    ans = "1\(ans)"
+                    carried = false
+                } else {
+                    ans = "0\(ans)"
+                    carried = false
+                }
+            }
+            indexA -= 1
+            indexB -= 1
+        }
+        if carried {
+            ans = "1\(ans)"
+        }
         return ans
     }
     
 }
+
+let solution = Solution()
+print(solution.addBinary("1010", "1011"))
