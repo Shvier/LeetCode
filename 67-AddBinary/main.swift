19b44863a30e353302d9922f6c66b28df5a5c4ac
Shvier <daulvi@hotmail.com>
2018-01-30 23:45:02 +0800

    Add Problem 67 Save

---
+++ 67-AddBinary/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  67-AddBinary
+//
+//  Created by Shvier on 29/01/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func addBinary(_ a: String, _ b: String) -> String {
+        let charactersA = [Character](a.characters)
+        let charactersB = [Character](b.characters)
+        let lengthA = charactersA.count
+        let lengthB = charactersB.count
+        var ans = ""
+        
+        return ans
+    }
+    
+}
