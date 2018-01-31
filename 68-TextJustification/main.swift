8d0f7e59810650bc83d1f00dfc2f7ab065309d90
Shvier <daulvi@hotmail.com>
2018-01-31 23:46:38 +0800

    Add Problem 68 Save

---
+++ 68-TextJustification/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  68-TextJustification
+//
+//  Created by Shvier on 31/01/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func fullJustify(_ words: [String], _ maxWidth: Int) -> [String] {
+        
+    }
+    
+}
