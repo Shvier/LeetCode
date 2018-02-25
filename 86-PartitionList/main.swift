db9a418f42ee3e37736f7f29eed7af707522de85
Shvier <daulvi@hotmail.com>
2018-02-25 23:55:14 +0800

    Add Problem 86

---
+++ 86-PartitionList/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  86-PartitionList
+//
+//  Created by Shvier on 25/02/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    
+    func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        
+    }
+    
+}
