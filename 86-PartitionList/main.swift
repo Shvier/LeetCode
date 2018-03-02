15f75d6f49bfd24335fc8de9fd5d1358fc94e096
Shvier <daulvi@hotmail.com>
2018-03-02 16:22:16 +0800

    Update Problem 86 Solution

--- 86-PartitionList/main.swift
+++ 86-PartitionList/main.swift
@@ -21,7 +21,38 @@ public class ListNode {
 class Solution {
     
     func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
-        
+        guard let newHead = head else { return  head }
+        if head?.next == nil {
+            return head
+        }
+        let ans = ListNode.init(0)
+        var p = ListNode.init(0)
+        var q = ListNode.init(0)
+        let pHead = p
+        let qHead = q
+        var pEnd = p
+        var temp = head
+        while temp != nil {
+            let t = ListNode.init(temp!.val)
+            if temp!.val < x {
+                p.next = t
+                p = p.next!
+                pEnd = p
+            } else {
+                q.next = t
+                q = q.next!
+            }
+            temp = temp!.next
+        }
+        pEnd.next = qHead.next
+        ans.next = pHead.next
+        return ans.next
     }
     
 }
+
+let node1 = ListNode.init(1)
+let node2 = ListNode.init(1)
+node1.next = node2
+let solution = Solution()
+print(solution.partition(node1, 2))
