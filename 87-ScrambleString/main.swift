ac90d5d9391db0741762afc097dfb78e2486308f
Shvier <daulvi@hotmail.com>
2018-03-03 22:34:57 +0800

    Add Problem 87

---
+++ 87-ScrambleString/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  87-ScrambleString
+//
+//  Created by Shvier on 02/03/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func isScramble(_ s1: String, _ s2: String) -> Bool {
+        
+    }
+    
+}
