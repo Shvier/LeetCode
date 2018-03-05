17c9ff0e05fee7044fd5561ac3d884bab64377a2
Shvier <daulvi@hotmail.com>
2018-03-05 11:48:55 +0800

    Update Problem 88 Solution

--- 88-MergeSortedArray/main.swift
+++ 88-MergeSortedArray/main.swift
@@ -11,25 +11,30 @@ import Foundation
 class Solution {
     
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var position = 0
-        for number in nums2 {
-            if number < nums1[position] {
-                nums1.insert(number, at: position)
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
+        while i >= 0 && j >= 0 {
+            if nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+                k -= 1
             } else {
-                if position != nums1.count-1 {
-                    for index in position..<nums1.count {
-                        if number < nums1[index] {
-                            nums1.insert(number, at: index)
-                            position = index
-                            break
-                        }
-                    }
-                    position = nums1.count-1
-                } else {
-                    nums1.append(number)
-                }
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
             }
+        }
+        while j >= 0 {
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
         }
     }
     
 }
+
+let solution = Solution()
+var nums1 = [1, 2, 3, 0, 0, 0]
+var nums2 = [2, 5, 6]
+print(solution.merge(&nums1, 3, nums2, 3))
