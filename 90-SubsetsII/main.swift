8986679a6accaab27aba6a11cbf421eef5ea1883
Shvier <daulvi@hotmail.com>
2018-03-08 23:37:32 +0800

    Update Need bug fix

--- 90-SubsetsII/main.swift
+++ 90-SubsetsII/main.swift
@@ -13,19 +13,28 @@ class Solution {
     func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
         var ans = [[Int]]()
         var temp = [Int]()
-        subset(&ans, nums, &temp, 0, nums.count)
+        ans.append(temp)
+        let count = nums.count
+        for index in 0..<count {
+            if index > 0 && nums[index] == nums[index-1] {
+                continue
+            }
+            temp.removeAll()
+            subset(&ans, nums, &temp, index, index+1, count)
+        }
         return ans
     }
     
-    func subset(_ ans: inout [[Int]], _ nums: [Int], _ temp: inout [Int], _ current: Int, _ max: Int) {
+    func subset(_ ans: inout [[Int]], _ nums: [Int], _ temp: inout [Int], _ start: Int, _ current: Int, _ max: Int) {
+        temp.append(nums[start])
         ans.append(temp)
-        if current >= max {
-            return
-        }
-        for index in current..<max {
-            temp.append(nums[index])
-            subset(&ans, nums, &temp, index+1, max)
-            temp.removeLast()
+        for i in current..<max {
+            for j in i..<max {
+                temp.append(nums[j])
+                ans.append(temp)
+            }
+            temp.removeAll()
+            temp.append(nums[start])
         }
     }
     
