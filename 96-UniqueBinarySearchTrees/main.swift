6666332d3efc311524a278ca2a425efc6e5fee6f
Shvier <daulvi@hotmail.com>
2018-03-12 17:42:11 +0800

    Add Problem 96 Solution

---
+++ 96-UniqueBinarySearchTrees/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  96-UniqueBinarySearchTrees
+//
+//  Created by Shvier on 12/03/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func numTrees(_ n: Int) -> Int {
+        var ans = Array(repeating: 0, count: n+1)
+        ans[0] = 1
+        ans[1] = 1
+        for i in 2...n {
+            for j in 1...i {
+                ans[i] += ans[j-1]*ans[i-j]
+            }
+        }
+        return ans[n]
+    }
+    
+}
