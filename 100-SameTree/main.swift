4ed31107d04b6be28182ff94e26f648897d7fa2d
Shvier <daulvi@hotmail.com>
2018-03-16 18:10:18 +0800

    Add Problem 100

---
+++ 100-SameTree/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  100-SameTree
+//
+//  Created by Shvier on 16/03/2018.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        
+    }
+    
+}
