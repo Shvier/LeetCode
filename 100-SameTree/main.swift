45d4abe69484539845b1386b81a9c83240023009
Shvier <daulvi@hotmail.com>
2018-03-18 11:41:40 +0800

    Update Problem 100 Solution

--- 100-SameTree/main.swift
+++ 100-SameTree/main.swift
@@ -23,7 +23,16 @@ public class TreeNode {
 class Solution {
     
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-        
+        if p == nil && q == nil {
+            return true
+        }
+        if p == nil || q == nil {
+            return false
+        }
+        if p?.val != q?.val {
+            return false
+        }
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
     }
     
 }
