c3a1da0db4a45f9978b211cea83bbb5d6f17b5ed
Shvier <daulvi@hotmail.com>
2018-03-18 14:09:05 +0800

    Update Problem 99 Solution Save for test

--- 99-RecoverBinarySearchTree/main.swift
+++ 99-RecoverBinarySearchTree/main.swift
@@ -23,7 +23,47 @@ public class TreeNode {
 class Solution {
     
     func recoverTree(_ root: TreeNode?) {
-        
+        var cur = root
+        var prev: TreeNode?
+        var temp: TreeNode?
+        var p1: TreeNode?
+        var p2: TreeNode?
+        while cur != nil {
+            if cur?.left == nil {
+                if temp != nil && temp!.val > cur!.val {
+                    if p1 == nil {
+                        p1 = temp
+                    }
+                    p2 = cur
+                }
+                temp = root
+                cur = cur?.right
+            } else {
+                prev = cur?.left
+                while prev?.right != nil && prev?.right !== cur {
+                    prev = prev?.right
+                }
+                if prev?.right == nil {
+                    prev?.right = cur
+                    cur = cur?.left
+                } else {
+                    if temp != nil && temp!.val > cur!.val {
+                        if p1 == nil {
+                            p1 = temp
+                        }
+                        p2 = cur
+                    }
+                    temp = root
+                    prev?.right = nil
+                    cur = cur?.right
+                }
+            }
+        }
+        if p1 != nil && p2 != nil {
+            let temp = p1!.val
+            p1!.val = p2!.val
+            p2!.val = temp
+        }
     }
     
 }
