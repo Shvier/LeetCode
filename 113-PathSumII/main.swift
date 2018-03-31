6ec548eb7a9d3a6a53f979b332855ff98694fb46
Shvier <daulvi@hotmail.com>
2018-03-31 20:54:38 +0800

    Add Problem 113 Solution Save for test

---
+++ 113-PathSumII/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  113-PathSumII
+//
+//  Created by Shvier on 2018/3/30.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    
+    func pathSum(_ root: TreeNode?, _ sum: Int) -> [[Int]] {
+        var ans = [[Int]]()
+        var temp = [Int]()
+        searchPath(&ans, &temp, root, sum)
+        return ans
+    }
+    
+    func searchPath(_ ans: inout [[Int]], _ temp: inout [Int], _ root: TreeNode?, _ sum: Int) {
+        if sum == 0 {
+            ans.append(temp)
+            return
+        }
+        if root == nil {
+            return
+        }
+        temp.append(root!.val)
+        searchPath(&ans, &temp, root?.left, sum - root!.val)
+        searchPath(&ans, &temp, root?.right, sum - root!.val)
+        temp.removeLast()
+    }
+
+}
