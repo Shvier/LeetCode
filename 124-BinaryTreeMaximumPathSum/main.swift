97c9b870120eac4fc1d6ddae936b9f83954873e3
Shvier <daulvi@hotmail.com>
2018-05-23 22:58:18 +0800

    Add Problem 124 Need bug fix

---
+++ 124-BinaryTreeMaximumPathSum/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  124-BinaryTreeMaximumPathSum
+//
+//  Created by Shvier on 2018/5/22.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    
+    func maxPathSum(_ root: TreeNode?) -> Int {
+        var maxValue = Int.min
+        return maxPathDown(root, &maxValue)
+    }
+    
+    func maxPathDown(_ root: TreeNode?, _ maxValue: inout Int) -> Int {
+        if root == nil {
+            return 0
+        }
+        let left = max(0, maxPathDown(root?.left, &maxValue))
+        let right = max(0, maxPathDown(root?.right, &maxValue))
+        maxValue = max(maxValue, left+right+root!.val)
+        return max(left, right) + root!.val
+    }
+    
+}
