4e25bed34e5db9a4da33f0d2b5cfd6f9185a5368
Shvier <daulvi@hotmail.com>
2018-05-27 15:02:17 +0800

    Update Problem 126 Save

--- 126-WordLadderII/main.swift
+++ 126-WordLadderII/main.swift
@@ -12,31 +12,7 @@ class Solution {
     
     func findLadders(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
         var ans = [[String]]()
-        var temp = [String]()
-        if wordList.contains(endWord) {
-            temp.append(beginWord)
-            match(beginWord, endWord, wordList, &ans, &temp)
-        }
         return ans
-    }
-    
-    func match(_ beginWord: String, _ endWord: String, _ wordList: [String], _ ans: inout [[String]], _ temp: inout [String]) {
-        if canTransform(beginWord, endWord) {
-            if temp.count > 0 && temp.count == endWord.lengthOfBytes(using: .utf8) + 1 {
-                temp.append(endWord)
-                ans.append(temp)
-                temp.removeLast()
-            }
-            return
-        }
-        for i in 0..<wordList.count {
-            if !canTransform(beginWord, wordList[i]) || temp.contains(wordList[i]) {
-                continue
-            }
-            temp.append(wordList[i])
-            match(wordList[i], endWord, wordList, &ans, &temp)
-            temp.removeLast()
-        }
     }
     
     func canTransform(_ string1: String, _ string2: String) -> Bool {
@@ -52,7 +28,72 @@ class Solution {
         return countOfDifference == count-1
     }
     
+    func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+        if beginWord == endWord {
+            return 1
+        }
+        var dict = Set<String>()
+        for word in wordList {
+            dict.insert(word)
+        }
+        var visited = Set<String>()
+        var list = [String]()
+        visited.insert(beginWord)
+        list.append(beginWord)
+        var length = 1
+        var ladder = [[String: [String]]]()
+        var minStep = Int.max
+        while !list.isEmpty {
+            length = length + 1
+            let count = list.count
+            print("length: \(length) - count: \(count)")
+            for _ in 0..<count {
+                let word = list.removeFirst()
+                let nextWords = getNextWords(word, dict)
+                print("word: \(word)\nnextWords: \(nextWords)")
+                var tempDict = [String: [String]]()
+                tempDict.updateValue(nextWords, forKey: word)
+                ladder.append(tempDict)
+                for nextWord in nextWords {
+                    if visited.contains(nextWord) {
+                        continue
+                    }
+                    if nextWord == endWord && length <= minStep {
+                        minStep = length
+//                        return length
+                    }
+                    visited.insert(nextWord)
+                    list.append(nextWord)
+                }
+            }
+        }
+        print(ladder)
+        return 0
+    }
+    
+    func getNextWords(_ word: String, _ dict: Set<String>) -> [String] {
+        var nextWords = [String]()
+        let startingLetter = Int(("a" as UnicodeScalar).value)
+        var characters = [Character](word.characters)
+        for i in 0..<characters.count {
+            let currentChar = characters[i]
+            for j in 0..<26 {
+                let letter = Character(UnicodeScalar(j + startingLetter)!)
+                if currentChar == letter {
+                    continue
+                }
+                characters[i] = letter
+                let nextString = String(characters)
+                if dict.contains(nextString) {
+                    nextWords.append(nextString)
+                }
+                characters[i] = currentChar
+            }
+        }
+        return nextWords
+    }
+    
 }
 
 let solution = Solution()
-print(solution.findLadders("a", "c", ["a", "b", "c"]))
+print(solution.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
