d813e77944563ba9cc977e774abaad69142e7c1e
Shvier <daulvi@hotmail.com>
2018-05-29 22:48:35 +0800

    Update Problem 129 Solution

--- 129-SumRoottoLeafNumbers/main.swift
+++ 129-SumRoottoLeafNumbers/main.swift
@@ -24,44 +24,41 @@ class Solution {
     
     func sumNumbers(_ root: TreeNode?) -> Int {
         var ans = 0
-        var temp = [Int]()
+        var temp = ""
         dfs(root, &ans, &temp)
         return ans
     }
     
-    func dfs(_ root: TreeNode?, _ ans: inout Int, _ temp: inout [Int]) {
+    func dfs(_ root: TreeNode?, _ ans: inout Int, _ temp: inout String) {
         if root == nil {
             return
         }
         if root?.left == nil && root?.right == nil {
-            temp.append(root!.val)
-            let count = temp.count
-            for index in 0..<count {
-                ans = ans + (temp[index] == 0 ? 0 : temp[index]*Int(pow(10, Double(count-index-1))))
-            }
-            temp.removeLast()
+            temp = "\(temp)\(root!.val)"
+            ans = ans + Int(temp)!
+            temp = temp.substring(to: temp.index(temp.startIndex, offsetBy: temp.lengthOfBytes(using: .utf8)-1))
             return
         }
-        temp.append(root!.val)
-        dfs(root?.left, &ans, &temp)
-        dfs(root?.right, &ans, &temp)
-        temp.removeLast()
+        temp = "\(temp)\(root!.val)"
+        dfs(root!.left, &ans, &temp)
+        dfs(root!.right, &ans, &temp)
+        temp = temp.substring(to: temp.index(temp.startIndex, offsetBy: temp.lengthOfBytes(using: .utf8)-1))
     }
     
 }
 
-//let node1 = TreeNode.init(4)
-//let node2 = TreeNode.init(9)
-//let node3 = TreeNode.init(0)
-//let node4 = TreeNode.init(5)
-//let node5 = TreeNode.init(1)
-//node1.left = node2
-//node1.right = node3
-//node2.left = node4
-//node2.right = node5
-
-let node1 = TreeNode.init(0)
-let node2 = TreeNode.init(1)
+let node1 = TreeNode.init(4)
+let node2 = TreeNode.init(9)
+let node3 = TreeNode.init(0)
+let node4 = TreeNode.init(5)
+let node5 = TreeNode.init(1)
 node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+
+//let node1 = TreeNode.init(0)
+//let node2 = TreeNode.init(1)
+//node1.left = node2
 let solution = Solution()
 print(solution.sumNumbers(node1))
