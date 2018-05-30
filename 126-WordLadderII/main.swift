789974f524f2ff309ac72b357ca82706ac9b9107
Shvier <daulvi@hotmail.com>
2018-05-30 23:25:31 +0800

    Update Problem 126 Solution but TLE Add Problem 131. Need code

--- 126-WordLadderII/main.swift
+++ 126-WordLadderII/main.swift
@@ -12,88 +12,62 @@ class Solution {
     
     func findLadders(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
         var ans = [[String]]()
-        return ans
-    }
-    
-    func canTransform(_ string1: String, _ string2: String) -> Bool {
-        let characters1 = [Character](string1.characters)
-        let characters2 = [Character](string2.characters)
-        let count = characters1.count
-        var countOfDifference = 0
-        for index in 0..<count {
-            if characters1[index] == characters2[index] {
-                countOfDifference = countOfDifference + 1
-            }
-        }
-        return countOfDifference == count-1
-    }
-    
-    func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
-        if beginWord == endWord {
-            return 1
-        }
         var dict = Set<String>()
         for word in wordList {
             dict.insert(word)
         }
-        var visited = Set<String>()
-        var list = [String]()
-        visited.insert(beginWord)
-        list.append(beginWord)
-        var length = 1
-        var ladder = [[String: [String]]]()
-        var minStep = Int.max
-        while !list.isEmpty {
-            length = length + 1
-            let count = list.count
-            print("length: \(length) - count: \(count)")
-            for _ in 0..<count {
-                let word = list.removeFirst()
-                let nextWords = getNextWords(word, dict)
-                print("word: \(word)\nnextWords: \(nextWords)")
-                var tempDict = [String: [String]]()
-                tempDict.updateValue(nextWords, forKey: word)
-                ladder.append(tempDict)
-                for nextWord in nextWords {
-                    if visited.contains(nextWord) {
+        var current = [String]()
+        current.append(beginWord)
+        var paths = [[String]]()
+        paths.append(current)
+        var level = 1
+        var minLevel = Int.max
+        var words = Set<String>()
+        let startingLetter = Int(("a" as UnicodeScalar).value)
+        while !paths.isEmpty {
+            let tmp = paths.removeFirst()
+            if tmp.count > level {
+                for word in words {
+                    dict.remove(word)
+                }
+                words.removeAll()
+                level = tmp.count
+                if level > minLevel {
+                    break
+                }
+            }
+            var last = tmp.last!
+            var characters = [Character](last.characters)
+            for i in 0..<characters.count {
+                let currentChar = characters[i]
+                for j in 0..<26 {
+                    let letter = Character(UnicodeScalar(j + startingLetter)!)
+                    characters[i] = letter
+                    let nextString = String(characters)
+                    if !dict.contains(nextString) {
+                        characters[i] = currentChar
                         continue
                     }
-                    if nextWord == endWord && length <= minStep {
-                        minStep = length
-//                        return length
+                    words.insert(nextString)
+                    var newTemp = [String]()
+                    for string in tmp {
+                        newTemp.append(string)
                     }
-                    visited.insert(nextWord)
-                    list.append(nextWord)
+                    newTemp.append(nextString)
+                    if nextString == endWord {
+                        ans.append(newTemp)
+                        minLevel = level
+                    } else {
+                        paths.append(newTemp)
+                    }
+                    characters[i] = currentChar
                 }
             }
         }
-        print(ladder)
-        return 0
-    }
-    
-    func getNextWords(_ word: String, _ dict: Set<String>) -> [String] {
-        var nextWords = [String]()
-        let startingLetter = Int(("a" as UnicodeScalar).value)
-        var characters = [Character](word.characters)
-        for i in 0..<characters.count {
-            let currentChar = characters[i]
-            for j in 0..<26 {
-                let letter = Character(UnicodeScalar(j + startingLetter)!)
-                if currentChar == letter {
-                    continue
-                }
-                characters[i] = letter
-                let nextString = String(characters)
-                if dict.contains(nextString) {
-                    nextWords.append(nextString)
-                }
-                characters[i] = currentChar
-            }
-        }
-        return nextWords
+        return ans
     }
     
 }
 
 let solution = Solution()
-print(solution.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
+print(solution.findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
---
+++ 131-PalindromePartitioning/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  131-PalindromePartitioning
+//
+//  Created by Shvier on 2018/5/30.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func partition(_ s: String) -> [[String]] {
+        
+    }
+    
+}
