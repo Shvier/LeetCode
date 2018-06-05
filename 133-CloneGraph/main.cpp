da574efbcdc568aab02d4f57e5d160a97be88e21
Shvier <daulvi@hotmail.com>
2018-06-05 12:39:12 +0800

    Add Problem 133 Solution

--- 133-CloneGraph/main.cpp
+++ 133-CloneGraph/main.cpp
@@ -7,25 +7,49 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
 
 using namespace std;
 
 // Definition for undirected graph.
 struct UndirectedGraphNode {
     int label;
-    vector<UndirectedGraphNode *> *neighbors;
+    vector<UndirectedGraphNode *> neighbors;
     UndirectedGraphNode(int x) : label(x) {};
 };
 
 class Solution {
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
-        
+        if (node == nullptr) {
+            return node;
+        }
+        auto cloneNode = new UndirectedGraphNode(node->label);
+        stack<UndirectedGraphNode *> nodeStack;
+        nodeStack.push(node);
+        unordered_map<int, UndirectedGraphNode *> doneLabels;
+        doneLabels[cloneNode->label] = cloneNode;
+        while (!nodeStack.empty()) {
+            auto topNode = nodeStack.top();
+            nodeStack.pop();
+            for (auto node : topNode->neighbors) {
+                if (doneLabels.find(node->label) == doneLabels.end()) {
+                    auto newNode = new UndirectedGraphNode(node->label);
+                    doneLabels[topNode->label]->neighbors.push_back(newNode);
+                    nodeStack.push(node);
+                    doneLabels[newNode->label] = newNode;
+                } else {
+                    doneLabels[topNode->label]->neighbors.push_back(doneLabels[node->label]);
+                }
+            }
+        }
+        return cloneNode;
     }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    Solution *solution = new Solution()
     return 0;
 }
