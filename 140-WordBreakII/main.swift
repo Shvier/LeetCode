e2a86a46ade5e7fb268830993d65596debcca75f
Shvier <daulvi@hotmail.com>
2018-06-14 16:11:10 +0800

    Update Problem 140 Solution

--- 140-WordBreakII/main.swift
+++ 140-WordBreakII/main.swift
@@ -11,12 +11,28 @@ import Foundation
 class Solution {
     
     func wordBreak(_ s: String, _ wordDict: [String]) -> [String] {
-        var ans = [String]()
-        var temp = [String]()
-        let characters = [Character](s.characters)
-        let length = characters.count
-        dfs(&ans, &temp, characters, wordDict, length, 0)
-        return ans
+        var map = [String : [String]]()
+        return dfs2(s, wordDict, &map)
+//        var ans = [String]()
+//        var temp = [String]()
+//        let characters = [Character](s.characters)
+//        let length = characters.count
+//        
+//        var dp = Array(repeating: false, count: length + 1)
+//        dp[0] = true
+//        for i in 1...length {
+//            for j in 0..<i {
+//                let temp = String(characters[j..<i])
+//                if dp[j] && wordDict.contains(temp) {
+//                    dp[i] = true
+//                    break
+//                }
+//            }
+//        }
+//        if dp[length] {
+//            dfs(&ans, &temp, characters, wordDict, length, 0)
+//        }
+//        return ans
     }
     
     func dfs(_ ans: inout [String], _ temp: inout [String], _ characters: [Character], _ wordDict: [String], _ length: Int, _ startIndex: Int) {
@@ -28,8 +44,8 @@ class Solution {
             ans.append(string)
             return
         }
-        for j in startIndex...length {
-            let tempString = String(characters[startIndex..<j])
+        for index in startIndex...length {
+            let tempString = String(characters[startIndex..<index])
             if wordDict.contains(tempString) {
                 temp.append(tempString)
                 dfs(&ans, &temp, characters, wordDict, length, startIndex + tempString.lengthOfBytes(using: .utf8))
@@ -38,7 +54,51 @@ class Solution {
         }
     }
     
+    func wordBreak2(_ s: String, _ wordDict: [String]) -> [String] {
+        var ans = [String]()
+        var temp = [String]()
+        let characters = [Character](s.characters)
+        let length = characters.count
+        
+        var dp = Array(repeating: false, count: length + 1)
+        dp[0] = true
+        for i in 1...length {
+            for j in 0..<i {
+                let temp = String(characters[j..<i])
+                if dp[j] && wordDict.contains(temp) {
+                    dp[i] = true
+                    break
+                }
+            }
+        }
+        if dp[length] {
+            dfs(&ans, &temp, characters, wordDict, length, 0)
+        }
+        return ans
+    }
+    
+    func dfs2(_ string: String, _ wordDict: [String], _ map: inout [String: [String]]) -> [String] {
+        if map.keys.contains(string) {
+            return map[string]!
+        }
+        var res = [String]()
+        if string.lengthOfBytes(using: .utf8) == 0 {
+            res.append("")
+            return res
+        }
+        for word in wordDict {
+            if string.hasPrefix(word) {
+                let subList = dfs2(string.substring(from: string.index(string.startIndex, offsetBy: word.lengthOfBytes(using: .utf8))), wordDict, &map)
+                for sub in subList {
+                    res.append("\(word)\(sub.isEmpty ? "" : " ")\(sub)")
+                }
+            }
+        }
+        map.updateValue(res, forKey: string)
+        return res
+    }
+    
 }
 
 let solution = Solution()
-print(solution.wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+print(solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
